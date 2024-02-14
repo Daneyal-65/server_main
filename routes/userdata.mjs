2d7fcef79e6a15f9c7a29ce2e9a449c9a091378b
daneyal <danixchacha@gmail.com>
2024-02-14 22:19:45 +0530

    first

---
+++ routes/userdata.mjs
@@ -0,0 +1,61 @@
+import express from "express";
+import db from "../db/conn.mjs";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+
+
+// signUP request handle
+  router.post("/signup", async (req, res) => {
+    try {
+      const collection = await db.collection("userdata");
+      const registerData = req.body;
+      registerData.date = new Date();
+      const result = await collection.insertOne(registerData);
+      result.redirectUrl='/login'
+      res.json(result); // Send back the inserted document
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  
+  router.post("/habit", async (req, res) => {
+    try {
+      const collection = await db.collection("userhabits");
+      const registerData = req.body;
+      registerData.date = new Date();
+      const result = await collection.insertOne(registerData);
+      res.json(result); // Send back the inserted document
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+//   
+
+// login request handled
+
+// 
+  router.get('/login',async (req,res)=>{
+ try{
+    const collection = await db.collection("userdata");
+    const {userName,password} = req.query;
+    const user = await collection.findOne({userName,password});
+    if(user){
+        res.json({message:"successfull",ok:true,redirectUrl:"/"});
+    }
+    else{
+        console.log("invalid input");
+        res.json({message:"username or password incorrect!!!",ok:false});
+    }
+ }catch (err){
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+ }
+
+  })
+
+export default router;
